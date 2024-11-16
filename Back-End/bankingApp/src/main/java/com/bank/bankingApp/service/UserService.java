9b32085d805d82854e39262184d6e5063650760d
ZSHARIEF6 <19P1091@GMAIL.COM>
2024-11-16 22:55:47 +0530

    backend
    backend

---
+++ Back-End/bankingApp/src/main/java/com/bank/bankingApp/service/UserService.java
@@ -0,0 +1,27 @@
+package com.bank.bankingApp.service;
+
+import com.bank.bankingApp.model.User;
+import com.bank.bankingApp.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public User registerUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public Optional<User> loginUser(String email, String password) {
+        Optional<User> user = userRepository.findByEmail(email);
+        if (user.isPresent() && user.get().getPassword().equals(password)) {
+            return user;
+        }
+        return Optional.empty();
+    }
+}
